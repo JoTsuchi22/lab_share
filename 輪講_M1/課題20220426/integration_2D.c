844891ea17a160d54bb53c29f5edfc6506e66968
Tsuchi_note <78190918+JoTsuchi22@users.noreply.github.com>
2022-04-29 16:43:34 +0900

    commit

--- 輪講_M1/課題20220426/integration_2D.c
+++ 輪講_M1/課題20220426/integration_2D.c
@@ -24,32 +24,18 @@ double element_GP[GP_1D];
 FILE *fp;
 
 void Make_gauss_array();
-void Integration(int element_num);
+double det_2D(double a[4]);
 double J_2D();
+void Integration(int element_num);
 
 int main()
 {
-    int i, j, k, l;  
-    double scale = PI / 2.0;    // 定積分範囲 ex) 0 <= x <= 2 -> 2
+    int i, j, k, l;
 
     Make_gauss_array();
+    Integration();
 
-    for (i = 0; i < GP_1D; i++)
-    {
-        double n_int = i + 1;
-
-        char filename[256];
-        filename[0] = i + 1 + '0';
-        filename[1] = '.';
-        filename[2] = 't';
-        filename[3] = 'x';
-        filename[4] = 't';
-        filename[5] = '\0';
-        fp = fopen(filename, "w");
-        fprintf(fp, "積分点:    %d\n\n", i + 1);
-        fprintf(fp, "分割数  Error\n");
-        fclose(fp);
-    }
+    
 }
 
 
@@ -77,12 +63,101 @@ void Make_gauss_array()
 }
 
 
-double J_1D(double temp_delta_el)
+double J_2D(double temp_delta_el)
 {
-    return temp_delta_el / 2.0; // partial x / partial xi
+    int i, j, k;
+    double a[2][2];
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            a[i][j] = 0.0;
+        }
+    }
+
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            for (k = 0; k < 4; k++)
+            {
+                a[i][j] += X[k][i] * deriv;
+            }
+        }
+    }
+    return det_2D(a); // partial x / partial xi
+}
+
+double det_2D(double a[4])
+{
+    return a[0] * a[3] - a[1] * a[2];
+}
+
+double deriv(int axis, double xi, double eta)
+{
+    double result;
+    if (axis == 0) // partial N (xi, eta) / partial xi
+    {
+        if (num == 0)
+        {
+            result = - (1.0 - eta) / 4.0;
+        }
+        else if (num == 1)
+        {
+            result = (1.0 - eta) / 4.0;
+        }
+        else if (num == 2)
+        {
+            result = (1.0 + eta) / 4.0;
+        }
+        else if (num == 3)
+        {
+            result = - (1.0 + eta) / 4.0;
+        }
+    }
+    else if (axis == 1) // partial N (xi, eta) / partial eta
+    {
+        if (num == 0)
+        {
+            result = - (1.0 - xi) / 4.0;
+        }
+        else if (num == 1)
+        {
+            result = - (1.0 + xi) / 4.0;
+        }
+        else if (num == 2)
+        {
+            result = (1.0 + xi) / 4.0;
+        }
+        else if (num == 3)
+        {
+            result = (1.0 - xi) / 4.0;
+        }
+    }
+
+    return result;
 }
 
 void Integration(double *coordinete)
 {
+    int i, j;
+    double result = 0.0;
 
+    char filename = "result.txt";
+    fp = fopen(filename, "w");
+    fprintf(fp, "elementnum\tintegration_result\n");
+
+    for (i = 0; i < Element; i++)
+    {
+        double temp_result = 0.0;
+        for (j = 0; j < GP_2D; j++)
+        {
+            temp_result += J_2D() * w[j];
+        }
+        result += temp_result;
+        fprintf(fp, "%d\t%.15e", i, result);
+    }
+    fprintf(fp, "Total_integration_esult\n");
+    fprintf(fp, "%.15e", result);
+    fclose(fp);
 }
